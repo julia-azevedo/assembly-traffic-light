9209d8a0e2ccd2230232d3a39bede7281b24059d
julia-azevedo <jap@ic.ufal.br>
2025-09-27 19:51:11 -0300

    bugfix: fixing build bugs about Z registers

--- serial-communication/main.asm
+++ serial-communication/main.asm
@@ -1,318 +1,262 @@
-; código de envio para o arduino o status dos semáforos através da Serial
+ï»¿.include "m328Pdef.inc"
 
+; CÃ³digo de envio para o Arduino do status dos semÃ¡foros via Serial
+.equ F_CPU = 16000000
+.equ BAUD = 9600
+.equ UBRRVAL = ((F_CPU / (16 * BAUD)) - 1)
 
-.include "m328def.inc"
+; Registradores definidos na faixa r16-r31 para compatibilidade
+.def current_state_ser = r20
+.def time_remaining_ser = r21
+.def serial_temp = r18
+.def serial_temp2 = r19
 
-; velocidade clock 16MHz, 9600bps 
-; UBRRn = 16*10^6 / 16 * 9600 - 1
-; UBRRn = 103.1666 = 103
-
-.equ UBRRVAL = 103
-
-.def current_state_ser = r4 ; armazenar Estado Atual (0-9)
-.def serial_temp = r2 
-.def serial_temp2 = r3
-.def current_state_ser = r4 ; estado atual (backup)
-.def time_remaining_ser = r7 ; tempo restante (backup)
-
+; =====================
+; SEGMENTO DE CÃ“DIGO (Flash)
+; =====================
 .cseg
-
-; vetor de interrupção USART RX
-.org URX0addr
-	reti
-
-; definicao de strings constantes
-
-semaforo1: .db "S1:",0
-semaforo2: .db " S2:",0  
-semaforo3: .db " S3:",0
-semaforo4: .db " S4:",0
-timer_str: .db " Timer:",0
-segundos: .db "s",0
-vermelho: .db "VERMELHO",0
-verde: .db "VERDE",0
-amarelo: .db "AMARELO",0
-nova_linha: .db 13,10,0
+.org 0x00
+rjmp main
+.org URXCaddr
+reti
 
 ; ==========================
-; INICIALIZAÇÃO DA SERIAL 
-;===========================
+; Strings e Tabelas na Flash com alinhamento
+; ==========================
+semaforo1_str: .db "S1:",0,0 ; Alinhamento adicionado
+semaforo2_str: .db " S2:",0,0 ; Alinhamento adicionado
+semaforo3_str: .db " S3:",0,0 ; Alinhamento adicionado
+semaforo4_str: .db " S4:",0,0 ; Alinhamento adicionado
+timer_str: .db " Timer:",0,0 ; Alinhamento adicionado
+segundos: .db "s",0,0,0 ; Alinhamento adicionado
+nova_linha: .db 13,10,0
 
-usart_init:
-	push serial_temp
-
-	;config do baud rate (taxa de transmissao de bits/s)
-	ldi serial_temp, high(UBRRVAL)
-	sts UBRR0H, serial_temp
-	ldi serial_temp, low(UBRRVAL)
-	sts UBRR0L, serial_temp
-
-	;config do formato do frame
-	ldi serial_temp, (1 << UCSZ01) | (1 << UCSZ00)
-	sts UCSR0C, serial_temp
-
-	;habilitar transmissão TX
-	ldi serial_temp, (1 << TXEN0)
-	sts UCSR0B, serial_temp
-
-	pop serial_temp
-	ret
-
-; ==================================
-;  TRANSMISSÃO DE CARACTERE
-;==================================
-
-usart_transmit:
-	push serial_temp
-	push serial_temp2
-
-usart_transmit_wait:
-	lds serial_temp, UCSR0A
-	sbrs serial_temp, UDRE0
-	rjmp usart_transmit_wait
-
-	sts UDR0, r24
-
-	pop serial_temp2
-	pop serial_temp
-	ret
-
-; ==================================
-;  TRANSMISSÃO DE STRING
-;==================================
-
-usart_print_string:
-	push r24
-	push r30
-	push r31
-
-usart_print_loop:
-	lpm r24, Z+
-	cpi r24, 0
-	breq usart_print_end
-	rcall usart_transmit
-	rjmp usart_print_loop
-
-usart_print_end:
-	pop r31
-	pop r30
-	pop r24
-	ret
-
-; ==================================
-;  TRANSMISSÃO DE número (0-99)
-;==================================
-
-usart_print_number:
-	push serial_temp
-	push serial_temp2
-	push r24
-
-	mov serial_temp, r24 ; salva o número
-
-	; Dezenas
-	ldi serial_temp2, '0'
-
-
-usart_print_tens:
-
-	cpi serial_temp, 10
-	brlo usart_print_units
-	subi serial_temp, 10
-	inc serial_temp2
-	rjmp usart_print_tens
-
-; impressao das dezenas
-mov r24, serial_temp2
-rcall usart_transmit
-
-
-usart_print_units:
-	 
-	 mov r24, serial_temp
-	 subi r24, -'0'
-	 rcall usart_transmit
-
-	 pop r24
-	 pop serial_temp2
-	 pop serial_temp
-	 ret
-
-; ==================================
-;  DETERMINAR COR DO SEMÁFORO
-;==================================
-
-get_semaforo_color:
-	push serial_temp
-	push serial_temp2
-
-; Cálculo do índice: estado * 4 + (semaforo - 1)
-
-	mov serial_temp, current_state_ser
-	lsl serial_temp ; x2
-	lsl serial_temp ; x4
-	mov serial_temp2, r24
-	dec serial_temp2 ; semaforo - 1
-	add serial_temp, serial_temp2
-
-	; Ler da tabela de cores
-
-	ldi ZL, low(color_table*2)
-	ldi ZH, high(color_table*2)
-	add ZL, serial_temp
-	adc ZH, r1
-
-	lpm serial_temp, Z ; indice da cor (0, 1 ou 2)
-	; Apontar para string correspondente
-	ldi ZL, low(color_strings*2)
-	ldi ZH, high(color_strings*2)
-
-	; Cada string terá tamanho fixo (11 bytes)
-	ldi serial_temp2, 11
-	mul serial_temp, serial_temp2
-	add ZL, r0
-	adc ZH, r1
-
-	pop serial_temp2
-	pop serial_temp
-	ret
-
-; Tabela de mapeamento (10 estados x 4 semáforos)
+vermelho_str: .db "VERMELHO",0,0 ; 8 + 2 = 10 bytes (par)
+verde_str: .db "VERDE",0,0,0,0,0 ; 5 + 1 + 4 = 10 bytes (par)
+amarelo_str: .db "AMARELO",0,0,0 ; 7 + 1 + 2 = 10 bytes (par)
 
 color_table:
-; Est 0: S1-Verm(0), S2-Verde(1), S3-Verm(0), S4-Verde(1)
-.db 0, 1, 0, 1
-
-; Est 1: S1-Verm, S2-Verde, S3-Verm, S4-Amarelo(2)  
-.db 0, 1, 0, 2
-
-; Est 2: S1-Verm, S2-Verde, S3-Verm, S4-Verm
-.db 0, 1, 0, 0
-
-; Est 3: S1-Verm, S2-Amarelo, S3-Verm, S4-Verm
-.db 0, 2, 0, 0
-
-; Est 4: S1-Verm, S2-Verm, S3-Verm, S4-Verm
-.db 0, 0, 0, 0
-
-; Est 5: S1-Verm, S2-Verm, S3-Verde, S4-Verm
-.db 0, 0, 1, 0
-
-; Est 6: S1-Verde, S2-Verm, S3-Verde, S4-Verm
-.db 1, 0, 1, 0
-
-; Est 7: S1-Amarelo, S2-Verm, S3-Verde, S4-Verm
-.db 2, 0, 1, 0
-
-; Est 8: S1-Amarelo, S2-Verm, S3-Amarelo, S4-Verm
-.db 2, 0, 2, 0
-
-; Est 9: S1-Verm, S2-Verde, S3-Amarelo, S4-Verm
-.db 0, 1, 2, 0
+.db 0,1,0,1, 0,1,0,2, 0,1,0,0, 0,2,0,0, 0,0,0,0
+.db 0,0,1,0, 1,0,1,0, 2,0,1,0, 2,0,2,0, 0,1,2,0
 
 color_strings:
-	.db "VERMELHO", 0,0,0 ; 11 bytes
-	.db "VERDE", 0,0,0,0,0,0 
-	.db "AMARELO", 0,0,0,0
+.dw vermelho_str, verde_str, amarelo_str
 
-; =============================================
-; ENVIAR STATUS COMPLETO
-; =============================================
+; ==========================
+; InicializaÃ§Ã£o da Serial
+; ==========================
+usart_init:
+    push serial_temp
+    ldi serial_temp, high(UBRRVAL)
+    sts UBRR0H, serial_temp
+    ldi serial_temp, low(UBRRVAL)
+    sts UBRR0L, serial_temp
 
+    ldi serial_temp, (1<<UCSZ01)|(1<<UCSZ00)
+    sts UCSR0C, serial_temp
+
+    ldi serial_temp, (1<<TXEN0)
+    sts UCSR0B, serial_temp
+    pop serial_temp
+    ret
+
+; ==========================
+; TransmissÃ£o de caractere
+; Entrada: r24 contÃ©m o caractere a ser transmitido
+; ==========================
+usart_transmit:
+    push r16
+usart_transmit_wait:
+    lds r16, UCSR0A
+    sbrs r16, UDRE0
+    rjmp usart_transmit_wait
+    sts UDR0, r24
+    pop r16
+    ret
+
+; ==========================
+; TransmissÃ£o de string
+; Entrada: Z (r30:r31) contÃ©m o endereÃ§o inicial da string.
+; ==========================
+usart_print_string:
+    push r24
+    push r30
+    push r31
+    usart_print_loop:
+        lpm r24, Z+
+        cpi r24, 0
+        breq usart_print_end
+        rcall usart_transmit
+        rjmp usart_print_loop
+    usart_print_end:
+        pop r31
+        pop r30
+        pop r24
+        ret
+
+; ==========================
+; TransmissÃ£o de nÃºmero (0-99)
+; Entrada: r24 contÃ©m o nÃºmero
+; ==========================
+usart_print_number:
+    push serial_temp
+    push serial_temp2
+    mov serial_temp, r24
+    ldi serial_temp2, '0'
+usart_print_tens:
+    cpi serial_temp, 10
+    brlo usart_print_units
+    subi serial_temp, 10
+    inc serial_temp2
+    rjmp usart_print_tens
+usart_print_units:
+    mov r24, serial_temp2
+    rcall usart_transmit
+    mov r24, serial_temp
+    subi r24, -'0'
+    rcall usart_transmit
+    pop serial_temp2
+    pop serial_temp
+    ret
+
+; ==========================
+; Determinar cor do semÃ¡foro
+; Entrada: r24 = nÃºmero do semÃ¡foro (1-4)
+; SaÃ­da: Z (r30:r31) contÃ©m o endereÃ§o da string da cor
+; ==========================
+
+get_semaforo_color:
+    push r24
+    push r25
+    push r26
+    push r27
+    push r30
+    push r31
+
+    ; 1. Calcula o offset base na tabela (current_state_ser * 4)
+    mov r26, current_state_ser
+    lsl r26            ; *2
+    lsl r26            ; *4
+
+    ; Adiciona o Ã­ndice do semÃ¡foro (r24 = 1 a 4 â†’ -1 = 0 a 3)
+    mov r27, r24
+    dec r27
+    add r26, r27       ; r26 = offset final
+
+    ; Prepara Z = color_table + offset
+    ldi r30, low(color_table)
+    ldi r31, high(color_table)
+    add r30, r26
+    clr r27
+    adc r31, r27
+
+    lpm r24, Z         ; r24 = Ã­ndice da cor (0, 1, 2)
+
+    ; 2. Pega endereÃ§o da string da cor via tabela color_strings
+    ldi r30, low(color_strings*2)
+    ldi r31, high(color_strings*2)
+    lsl r24            ; r24 = r24 * 2 (Ã­ndice de palavra)
+    mov r26, r24
+    clr r27
+    add r30, r26
+    adc r31, r27
+
+    ; LÃª o ponteiro (endereÃ§o da string)
+    lpm r24, Z+        ; primeiro byte
+    lpm r25, Z         ; segundo byte
+    mov r30, r24
+    mov r31, r25       ; Z agora aponta para a string
+
+    pop r31
+    pop r30
+    pop r27
+    pop r26
+    pop r25
+    pop r24
+    ret
+; ==========================
+; Enviar status completo
+; ==========================
 send_semaforos_status:
-	push r24
-	push r30
-	push r31
+    push r24
+    push r25
+    push r30
+    push r31
+    
+    ; semÃ¡foro 1
+    ldi r30, low(semaforo1_str)
+    ldi r31, high(semaforo1_str)
+    rcall usart_print_string
+    ldi r24, 1
+    rcall get_semaforo_color
+    rcall usart_print_string
 
-	; semaforo 1
-	ldi ZL, low(semaforo1*2)
-	ldi ZH, high(semaforo1*2)
-	rcall usart_print_string
-	ldi r24, 1
-	rcall get_semaforo_color
-	rcall usart_print_string
-
-	; semaforo 2
-    ldi ZL, low(semaforo2*2)
-    ldi ZH, high(semaforo2*2)
+    ; semÃ¡foro 2
+    ldi r30, low(semaforo2_str)
+    ldi r31, high(semaforo2_str)
     rcall usart_print_string
     ldi r24, 2
     rcall get_semaforo_color
     rcall usart_print_string
 
-	; semaforo 3
-    ldi ZL, low(semaforo3*2)
-    ldi ZH, high(semaforo3*2)
+    ; semÃ¡foro 3
+    ldi r30, low(semaforo3_str)
+    ldi r31, high(semaforo3_str)
     rcall usart_print_string
     ldi r24, 3
     rcall get_semaforo_color
     rcall usart_print_string
-    
-    ; semaforo 4
-    ldi ZL, low(semaforo4*2)
-    ldi ZH, high(semaforo4*2)
+
+    ; semÃ¡foro 4
+    ldi r30, low(semaforo4_str)
+    ldi r31, high(semaforo4_str)
     rcall usart_print_string
     ldi r24, 4
     rcall get_semaforo_color
     rcall usart_print_string
 
-	; Timer
-    ldi ZL, low(timer_str*2)
-    ldi ZH, high(timer_str*2)
+    ; Timer
+    ldi r30, low(timer_str)
+    ldi r31, high(timer_str)
     rcall usart_print_string
     mov r24, time_remaining_ser
     rcall usart_print_number
-    ldi ZL, low(segundos*2)
-    ldi ZH, high(segundos*2)
+    ldi r30, low(segundos)
+    ldi r31, high(segundos)
     rcall usart_print_string
-    
-    ; Nova linha
-    ldi ZL, low(nova_linha*2)
-    ldi ZH, high(nova_linha*2)
+    ldi r30, low(nova_linha)
+    ldi r31, high(nova_linha)
     rcall usart_print_string
-    
-    pop r31
-    pop r30
-    pop r24
-    ret
 
-; =============================================
-; ATUALIZAR STATUS/ entrada: r24 = novo estado, r22 = tempo restante
-; =============================================
-
-
-serial_update_status:
-	mov current_state_ser, r24
-	mov time_remaining_ser, r22
-
-	; Preservar registradores críticos
-	push r16
-    push r17
-    push r18
-    push r19
-    push r20
-    push r21
-    push r22
-    push r23
-    push r24
-    push r25
-    push r30
-    push r31
-	
-	rcall send_semaforos_status
-
-    ; Restaurar registradores
     pop r31
     pop r30
     pop r25
     pop r24
-    pop r23
-    pop r22
-    pop r21
-    pop r20
-    pop r19
-    pop r18
-    pop r17
-    pop r16
-    
     ret
+
+; ==========================
+; Atualizar status
+; ==========================
+serial_update_status:
+    mov current_state_ser, r24
+    mov time_remaining_ser, r22
+    rcall send_semaforos_status
+    ret
+
+; ==========================
+; MAIN (exemplo)
+; ==========================
+main:
+    ; Configura o stack
+    ldi r16, high(RAMEND)
+    out SPH, r16
+    ldi r16, low(RAMEND)
+    out SPL, r16
+
+    rcall usart_init
+    ldi r24, 0
+    ldi r22, 15
+    rcall serial_update_status
+
+infinite_loop:
+    rjmp infinite_loop
